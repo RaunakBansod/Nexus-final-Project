dcf115ef825c239b2520a5a76a9588001fa4d4e7
RaunakBansod <157168225+RaunakBansod@users.noreply.github.com>
2024-02-10 21:53:18 +0530

    Final Project
    nexus final project

---
+++ Project.N.js
@@ -0,0 +1,12 @@
+
+// Get the element with id "search"
+var searchElement = document.getElementById('search');
+
+// Apply the CSS rule using the style property
+searchElement.style.width = '500px';
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the element with id "search"
+  var searchElement = document.getElementById("search");
+
+  searchElement.style.width = "500px";
+});
